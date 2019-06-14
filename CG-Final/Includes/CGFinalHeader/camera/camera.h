a35a13811c1d6b7475ba909056767b23136df5ad
Asd <asdiijj@21cn.com>
2019-06-14 23:43:43 +0800

    fix camera zoom

--- CG-Final/Includes/CGFinalHeader/camera/camera.h
+++ CG-Final/Includes/CGFinalHeader/camera/camera.h
@@ -26,7 +26,7 @@ const float YAW = -90.0f;
 const float PITCH = 0.0f;
 const float SPEED = 4.0f;
 const float SENSITIVITY = 0.1f;
-const float ZOOM = 100.0f; // 45.0f
+const float ZOOM = 45.0f;
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
 class Camera
